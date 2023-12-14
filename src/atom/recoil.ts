11caf31142aeb5a0a15ec3439562030a28acc035
nyoung03 <vnfdlvdk37@gmail.com>
2023-12-14 21:16:43 +0900

    일기 리스트 recoil로 상태관리

--- src/atom/recoil.ts
+++ src/atom/recoil.ts
@@ -14,3 +14,15 @@ export const diaryUpdateState = atom<boolean>({
   key: "diaryUpdateState",
   default: false,
 });
+
+interface IDiaryList {
+  id: number;
+  name: string;
+  createBy: string;
+  modifyBy: string;
+}
+
+export const diaryListState = atom<IDiaryList[]>({
+  key: "diaryListState",
+  default: [],
+});
