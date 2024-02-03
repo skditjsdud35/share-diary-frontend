701a84cf2f6b4d0c74776ca6a27066de32bcf01e
sunyoung <skditjsdud35@naver.com>
2024-02-04 02:07:02 +0900

    Feat: 비밀번호 재설정 개발 완료

--- src/component/FindMyInfo/SetPw.tsx
+++ src/component/FindMyInfo/SetPw.tsx
@@ -1,123 +1,137 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { Button, Form, Input, Modal } from 'antd';
+import React, { useState } from 'react';
+import { Form, Input } from 'antd';
 import axios from "axios";
 import DarkButton from '../Common/DarkButton'
+import { isPassword } from '../../utils/CheckValid';
 
 const SetPw = () => {
-    const [count, setCount] = useState<number | null>(null);    // 시간을 담을 변수
+    const [loginId, setLoginId] = useState('')
+    const [email, setEmail] = useState('')
+    const [certNumber, setCertNumber] = useState('')
+    const [password, setPassword] = useState('')
+    const [rePassword, setRePassword] = useState('')
+    const [showResetPw, setShowResetPw] = useState(false)
 
-
-    const clickFindBtn = () => {
-        // if (isFindId) {
-        //     axios({
-        //         method: "GET",
-        //         url: "/api/member/me/id",
-        //         params: {
-        //             email: text,
-        //         },
-        //     }).then((res) => {
-        //         alert(`회원님의 아이디는 ${res.data.loginId} 입니다`)
-        //     })
-        //         .catch(function (error) {
-        //             alert("회원을 찾을 수 없습니다.");
-        //         });
-        // }
-    };
-
-    //인증번호 발송 알림 모달
+    //인증번호 발송
     const sendCertNumber = () => {
-        Modal.success({
-            content: '인증번호를 발송했습니다',
-            onOk: () => {
-                startTimer();
+        axios({
+            method: "POST",
+            url: "/api/member/certification-number",
+            data: {
+                loginId: loginId,
+                email: email,
             },
-        });
-    };
+        }).then((res) => {
+            alert("인증번호를 발송했습니다.")
+        })
+            .catch(function (error) {
+                alert("인증번호 발송에 실패했습니다");
+            });
+    }
 
-    //타이머 시작 함수
-    const startTimer = () => {
-        setCount(180);
-    };
+    //인증번호 확인 
+    const chkCertNumber = () => {
+        axios({
+            method: "POST",
+            url: "/api/member/validation-certification-number",
+            data: {
+                certificationNumber: certNumber,
+            },
+        }).then((res) => {
+            alert("인증번호가 확인되었습니다.")
+            setShowResetPw(true)
+        })
+            .catch(function (error) {
+                alert("인증번호 확인에 실패했습니다");
+            });
+    }
 
-    //초 -> 분:초 변환 함수
-    const formatTime = (seconds: number): string => {
-        const min = Math.floor(seconds / 60);
-        const sec = seconds % 60;
-        return `${min} : ${sec}`;
-    };
+    //비밀번호 재설정
+    const resetPassword = () => {
 
-
-    useEffect(() => {
-        if (count !== null && count > 0) {
-            const id = setInterval(() => {
-                setCount((count) => (count ? count - 1 : 0));
-            }, 1000);
-
-            return () => clearInterval(id);
+        if (!password || !rePassword) {
+            alert('모든 필드를 입력해 주세요.');
+            return;
         }
-    }, [count]);
+
+        if (password !== rePassword) {
+            alert("비밀번호가 일치하지 않습니다.")
+            return;
+        }
+
+        if (!isPassword(password)) {
+            alert('필드를 형식에 맞추어 입력해 주세요.');
+            return;
+        }
+
+        axios({
+            method: "POST",
+            url: "/api/member/resetPassword",
+            data: {
+                //loginId: loginId,
+                password: certNumber,
+            },
+        }).then((res) => {
+            alert("비밀번호가 재설정되었습니다.")
+        })
+            .catch(function (error) {
+                alert("비밀번호 재설정에 실패했습니다.");
+            });
+    }
+
     return (
-        <Form name="form_item_path" layout="vertical">
-            <Form.Item
-                name="id"
-                label="아이디"
-                rules={[
-                    {
-                        required: true,
-                        message: '아이디를 입력해주세요',
-                    }
-                ]}
-            >
-                <div style={{ display: "flex" }}>
-                    <Input />
-                </div>
-            </Form.Item>
-            <Form.Item
-                name="email"
-                label="이메일"
-                rules={[
-                    {
-                        type: 'email',
-                        message: '유효하지 않은 이메일 형식입니다',
-                    },
-                    {
-                        required: true,
-                        message: '이메일을 입력해주세요',
-                    },
-                ]}
-            >
-                <div style={{ display: "flex" }}>
-                    <Input style={{ marginRight: "5px" }} />
-                    <DarkButton content="인증번호 발송" style={{ width: "150px" }} onClick={sendCertNumber} />
-                </div>
-            </Form.Item>
-            <Form.Item
-                name="number"
-                label="인증번호"
-                rules={[
-                    {
-                        required: true,
-                        message: '인증번호를 입력해주세요',
-                    },
-                ]}>
-                <div style={{ display: "flex" }}>
-                    <Input
-                        style={{ marginRight: "5px" }}
-                        suffix={<Timer>{count !== null ? formatTime(count) : ""}</Timer>}
-                    />
-                    <DarkButton content="확인" style={{ width: "150px" }} />
-                </div>
-            </Form.Item>
-            <DarkButton style={{ width: '100%' }} content="비밀번호 재설정" htmlType="submit" onClick={clickFindBtn} />
+        <Form layout="vertical">
+            {showResetPw ?
+                <>
+                    <Form.Item
+                        name="newPassword"
+                        label="새 비밀번호"
+                        rules={[{ required: true, message: '새로운 비밀번호를 입력해주세요' }]}>
+                        <div style={{ display: "flex" }}>
+                            <Input />
+                        </div>
+                    </Form.Item>
+                    <Form.Item
+                        name="reNewPassword"
+                        label="새 비밀번호 재입력"
+                        rules={[{ required: true, message: '새로운 비밀번호를 입력해주세요' }]}>
+                        <div style={{ display: "flex" }}>
+                            <Input />
+                        </div>
+                    </Form.Item>
+                    <DarkButton style={{ width: '100%' }} content="비밀번호 재설정" htmlType="submit" onClick={resetPassword} />
+                </> :
+                <>
+                    <Form.Item
+                        name="id"
+                        label="아이디"
+                        rules={[{ required: true, message: '아이디를 입력해주세요' }]}>
+                        <div style={{ display: "flex" }}>
+                            <Input />
+                        </div>
+                    </Form.Item>
+                    <Form.Item
+                        name="email"
+                        label="이메일"
+                        rules={[{ type: 'email', message: '유효하지 않은 이메일 형식입니다' }, { required: true, message: '이메일을 입력해주세요' }]}>
+                        <div style={{ display: "flex" }}>
+                            <Input style={{ marginRight: "5px" }} />
+                            <DarkButton content="인증번호 발송" style={{ width: "150px" }} onClick={sendCertNumber} />
+                        </div>
+                    </Form.Item>
+                    <Form.Item
+                        name="number"
+                        label="인증번호"
+                        rules={[{ required: true, message: '인증번호를 입력해주세요', }]}>
+                        <div style={{ display: "flex" }}>
+                            <Input style={{ marginRight: "5px" }} value={certNumber} onChange={e => setCertNumber(e.target.value)} />
+                        </div>
+                    </Form.Item>
+                    <DarkButton style={{ width: '100%' }} content="인증번호 확인" htmlType="submit" onClick={chkCertNumber} />
+                </>}
         </Form>
 
     )
 }
 
 export default SetPw
-
-const Timer = styled.span`
-  font-size: 12px;
-  margin-left: 8px;
-`;
