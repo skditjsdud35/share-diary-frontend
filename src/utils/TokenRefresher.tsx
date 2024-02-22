683e24e40ba1477e9b9888d01044635ca192851b
sunyoung <skditjsdud35@naver.com>
2024-02-22 17:27:20 +0900

    feat: backend url 설정

--- src/utils/TokenRefresher.tsx
+++ src/utils/TokenRefresher.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
     headers: { "Content-type": "application/json" }
 });
 
